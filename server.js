0a1ebb52c3e1410343b754684384a759716c555a
AlmonajedObada <obada_almonajed@hotmail.com>
2018-08-09 00:07:58 +0200

    heroku

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/static'));
-app.get('/', (req, res) => res.sendStatus(200));
 
 
 app.listen(port,function(){
