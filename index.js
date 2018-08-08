9ed29425c1e14635f44fe2d4dbf37e730e09e284
AlmonajedObada <obada_almonajed@hotmail.com>
2018-08-08 23:33:18 +0200

    .

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require('express')
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080
 
-var app = express()
+app.use(express.static(__dirname + '/static'));
 
-app.use('/', express.static(__dirname+"/static")
-)
 
-app.listen(8080, function(){
-    console.log('8080')
-})
+app.listen(port,function(){
+  console.log('Node app is running on port', port);
+});
