c2cc61139e6f86fb27bdec59608c9bca4d4cd3d0
AlmonajedObada <obada_almonajed@hotmail.com>
2018-08-12 15:01:55 +0200

    post

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ var db = mongojs('localhost:27017/Card', ['Kartica'])
 var body_parser = require('body-parser')
 app.use(body_parser.json())
 
+var urlencodedParser = body_parser.urlencoded({
+  extended: false
+})
+
 app.use(express.static(__dirname + '/static'))
 
 app.get('/Kartica', function (req, res) {
   db.Kartica.find(function (err, docs) {
+    res.json(docs)
+  })
+})
+
+app.post('/Kartica', urlencodedParser, function (req, res, next) {
+  console.log(req.body)
+
+  db.Kartica.insert(req.body, function (err, docs) {
+    console.log('Card date inserted successfully')
     res.json(docs)
   })
 })
--- static/controllers/cardGet.js
+++ static/controllers/cardGet.js
@@ -1,8 +1,31 @@
 function cardGet($scope, $http) {
-    httpGet();
-    function httpGet(){$http.get("/Kartica")
-    .then(function(response) {
-        $scope.myWelcome = response.data;
-    });
+    var refesh = function () {
+      $http.get('/Kartica').then(function (response) {
+        $scope.myWelcome = response.data
+      })
     }
-};
+  
+    refesh()
+  
+    $scope.addNewCard = function () {
+      console.log('Card added')
+      console.log($scope.card)
+      $http.post('/Kartica', $scope.card).then(function (response) {
+        console.log(response)
+        $scope.card.Name = ''
+        $scope.card.Description = ''
+        $scope.card.Image = ''
+        $scope.card.BarCode = ''
+        refesh()
+      })
+    }
+
+    $scope.show = function () {
+        $scope.visible = false;
+        $scope.visible = $scope.visible = true;
+    }
+    $scope.hide = function () {
+        $scope.visible = true;
+        $scope.visible = $scope.visible = false;
+    }
+}
